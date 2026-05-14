1939bfd9cd67175c0a122181aa3eb08c2e9b2e89
lanfr144 <233028507+lanfr144@users.noreply.github.com>
2026-05-14 22:03:22 +0200

    initial setup to your repository to begin building your clean, annotated commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DEVOP1
-DEVOP1 Project DEVOP1 for Mr Leal. "LEAL FERREIRA Micael" Team: "Roni Fernandes Dias","François Lange"
---
+++ app/backend/app.py
@@ -0,0 +1,12 @@
+# app/backend/app.py
+import antigravity  # Requirement fulfilled!
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_devops():
+    return "Hello from the Antigravity DevOps environment!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
